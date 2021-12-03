e08e0a9d6670e31dfe6977087d370f6c72881984
Gerald Yeo <ywz.gerald@gmail.com>
2021-12-03 14:55:19 +0800

    complete addToCart()

--- d1/src/main/java/swf/d1/FreshMart.java
+++ d1/src/main/java/swf/d1/FreshMart.java
@@ -3,6 +3,7 @@ package swf.d1;
 import java.io.Console;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class FreshMart {
 
@@ -13,24 +14,36 @@ public class FreshMart {
     }
 
     public boolean addToCart(String item) {
-        boolean addedToCart = false;
+
+        boolean addedToCart = true;
+
+        for(int i=0; i<cart.size();i++){
+            if(cart.get(i).equals(item)){
+                addedToCart = false;
+                break;
+            }
+        }
+
+        if(addedToCart){
+            cart.add(item);
+        }
 
         return addedToCart;
     }
 
-    private String greeting() {
+    public String greeting() {
         String greeting = "Welcome to your shopping cart!\n\n";
 
-        greeting += "1. List | List the contents of your cart.\n";
-        greeting += "2. Add <item1>, <item2> | Add item(s) to your cart.\n";
-        greeting += "3. Delete <index> | Remove an item from your cart.\n";
-        greeting += "4. Help | Show this menu.\n";
-        greeting += "5. Exit | Exit this program.\n\n";
+        greeting += "List \t\t\t| List the contents of your cart.\n";
+        greeting += "Add <item1>, <item2> \t| Add item(s) to your cart.\n";
+        greeting += "Delete <index> \t\t| Remove an item from your cart.\n";
+        greeting += "Help \t\t\t| Show this menu.\n";
+        greeting += "Exit \t\t\t| Exit this program.\n\n";
 
         return greeting;
     }
 
-    private String goodbye() {
+    public String goodbye() {
         String goodbye = "See you again!\n\n";
         return goodbye;
     }
@@ -39,7 +52,7 @@ public class FreshMart {
         String contents = "";
 
         if (cart.size() > 0) {
-            contents += "These items are currently in your cart.\n\n";
+            contents += "\nThese items are currently in your cart:\n\n";
             for (int i = 0; i < cart.size(); i++) {
                 contents += (i + ". " + cart.get(i) + "\n");
             }
@@ -74,7 +87,15 @@ public class FreshMart {
                     fm.print(fm.listCart());
                     break;
                 case "add":
-                    fm.print("");
+                    sc.useDelimiter(Pattern.compile("[\\p{Punct}*]"));
+                    while(sc.hasNext()){
+                        String newItem = sc.next().trim().toLowerCase();
+                        if(fm.addToCart(newItem)){
+                            fm.print(newItem + " has been added to your cart.");
+                        }else{
+                            fm.print(newItem + " already exists in cart.");
+                        }
+                    }
                     break;
                 case "delete":
                     fm.print("Command accepted.");
@@ -89,6 +110,7 @@ public class FreshMart {
                 default:
                     fm.print(command + " is not a valid command.");
             }
+            sc.close();
         }
     }
 }
