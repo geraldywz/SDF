c2dc6317b547f431cf2583ae60e1d76b55cdfb5e
Gerald Yeo <ywz.gerald@gmail.com>
2021-12-06 16:26:58 +0800

    Created FixedDepositAccount.

---
+++ D2/bank/src/main/java/swf/d2/FixedDepositAccount.java
@@ -0,0 +1,14 @@
+package swf.d2;
+
+public Class FixedDepositAccount extends BankAccount{
+    private float interest;
+    private int durationinMonths;
+
+    public FixedDepositAccount(){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
