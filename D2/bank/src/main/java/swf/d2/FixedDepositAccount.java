79b3b5e2ac676356666d1562ed30faeaa1a348a6
Gerald Yeo <ywz.gerald@gmail.com>
2021-12-07 10:23:36 +0800

    Getters/Setters fpr Interest/Duration.

--- D2/bank/src/main/java/swf/d2/FixedDepositAccount.java
+++ D2/bank/src/main/java/swf/d2/FixedDepositAccount.java
@@ -3,11 +3,15 @@ package swf.d2;
 public class FixedDepositAccount extends BankAccount{
     private float interest = 3f;
     private boolean isInterestModified = false;
-    private int durationinMonths = 6;
+    private int duration = 6;
     private boolean isDurationModified = false;
 
-    public FixedDepositAccount(){
+    public FixedDepositAccount(String name){
+        super(name);
+    }
 
+    public FixedDepositAccount(String name, float accountBalance){
+        super(name, accountBalance);
     }
 
     @Override
@@ -15,6 +19,38 @@ public class FixedDepositAccount extends BankAccount{
 
     @Override
     public boolean withdraw(float amount){return false;}
+
+    public boolean setDuration(int months) {
+        boolean success = true;
+        if (isDurationModified) {
+            success = false;
+            throw new IllegalArgumentException();
+        } else {
+            isDurationModified = true;
+            this.duration = months;
+        }
+        return success;
+    }
+
+    public int getDuration(){
+        return duration;
+    }
+    
+    public boolean setInterest(float amount) {
+        boolean success = true;
+        if (isInterestModified) {
+            success = false;
+            throw new IllegalArgumentException();
+        } else {
+            isInterestModified = true;
+            this.interest = amount;
+        }
+        return success;
+    }
+
+    public float getInterest(){
+        return interest;
+    }
 
     public static void main(String[] args) {
         
