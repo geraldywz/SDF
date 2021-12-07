d76232d5753b24e4f6aaa6bd3cc7566ed08707f1
Gerald Yeo <ywz.gerald@gmail.com>
2021-12-07 13:57:04 +0800

    Refactored FreshMart and ShoppingCart.

--- D3/cart/src/main/java/swf/d3/FreshMart.java
+++ D3/cart/src/main/java/swf/d3/FreshMart.java
@@ -1,41 +1,18 @@
 package swf.d3;
 
 import java.io.Console;
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class FreshMart {
 
-    private ArrayList<String> cart;
+    ShoppingCart cart;
 
     public FreshMart() {
-        cart = new ArrayList<String>();
+        cart = new ShoppingCart();
     }
 
-    public boolean addToCart(String item) {
-
-        boolean addedToCart = true;
-
-        for (int i = 0; i < cartSize(); i++) {
-            if (cart.get(i).equals(item)) {
-                addedToCart = false;
-                break;
-            }
-        }
-
-        if (addedToCart) {
-            cart.add(item);
-        }
-
-        return addedToCart;
-    }
-
-    public int cartSize() {
-        return cart.size();
-    }
-
-    public String greeting() {
+    public void greeting() {
         String greeting = "Welcome to Fresh Mart!\n\n";
 
         greeting += "List \t\t\t| List the contents of your cart.\n";
@@ -44,48 +21,58 @@ public class FreshMart {
         greeting += "Help \t\t\t| Show this menu.\n";
         greeting += "Exit \t\t\t| Exit this program.\n\n";
 
-        return greeting;
+        print(greeting);
     }
 
-    public String goodbye() {
+    public void goodbye() {
         String goodbye = "See you again!\n\n";
-        return goodbye;
+        print(goodbye);
     }
 
-    public String listCart() {
+    public void listCart() {
         String contents = "";
 
-        if (cartSize() > 0) {
+        if (cart.size() > 0) {
             contents += "\nThese items are currently in your cart:\n\n";
-            for (int i = 0; i < cartSize(); i++) {
+            for (int i = 0; i < cart.size(); i++) {
                 contents += ((i + 1) + ". " + cart.get(i) + "\n");
             }
         } else {
             contents += "Your cart is empty.\n";
         }
-        return contents;
+        print(contents);
     }
 
-    private void populateSampleData() {
-        addToCart("Apple");
-        addToCart("Orange");
-        addToCart("Banana");
-        addToCart("Pear");
-        addToCart("Kiwi");
-    }
-
-    public void print(String text) {
+    private void print(String text) {
         System.out.println(text);
     }
 
-    public String removeFromCart(int index) {
-        String removed = null;
-        if (index <= cartSize() && index > 0) {
-            index--;
-            removed = cart.get(index);
-            cart.remove(index);
+    public void processPurchase(Scanner sc) {
+        sc.useDelimiter(Pattern.compile("[\\p{Punct}*]"));
+        while (sc.hasNext()) {
+            String newItem = sc.next().trim().toLowerCase();
+            if (cart.addToCart(newItem)) {
+                print(newItem + " has been added to your cart.\n");
+            } else {
+                print(newItem + " already exists in cart.\n");
+            }
         }
-        return removed;
+    }
+
+    public void processRemoval(Scanner sc) {
+        if (sc.hasNextInt()) {
+            int index = sc.nextInt();
+            String removed = cart.removeFromCart(index);
+            if (removed != null) {
+                print(removed + " has been removed.\n");
+            } else {
+                print("PLease key in a valid index.");
+                print("Use the [list] command to see a list of current indices.\n");
+            }
+        } else {
+            print("Please key in the index of the item to remove after [delete].");
+            print("Use the [list] command to see a list of current indices.\n");
+        }
     }
 
     public static void main(String[] args) {
@@ -95,9 +82,8 @@ public class FreshMart {
         String input, command;
         Console cons = System.console();
         FreshMart fm = new FreshMart();
-        fm.populateSampleData();
 
-        fm.print(fm.greeting());
+        fm.greeting();
 
         while (martIsOpen) {
             input = cons.readLine();
@@ -106,39 +92,19 @@ public class FreshMart {
 
             switch (command) {
                 case "list":
-                    fm.print(fm.listCart());
+                    fm.listCart();
                     break;
                 case "add":
-                    sc.useDelimiter(Pattern.compile("[\\p{Punct}*]"));
-                    while (sc.hasNext()) {
-                        String newItem = sc.next().trim().toLowerCase();
-                        if (fm.addToCart(newItem)) {
-                            fm.print(newItem + " has been added to your cart.");
-                        } else {
-                            fm.print(newItem + " already exists in cart.");
-                        }
-                    }
+                    fm.processPurchase(sc);
                     break;
                 case "delete":
-                    if (sc.hasNextInt()) {
-                        int index = sc.nextInt();
-                        String removed = fm.removeFromCart(index);
-                        if (removed != null) {
-                            fm.print(removed + " has been removed.");
-                        } else {
-                            fm.print("PLease key in a valid index.");
-                            fm.print("Use the [list] command to see a list of current indices.\n");
-                        }
-                    } else {
-                        fm.print("Please key in the index of the item to remove after [delete].");
-                        fm.print("Use the [list] command to see a list of current indices.\n");
-                    }
+                    fm.processRemoval(sc);
                     break;
                 case "help":
-                    fm.print(fm.greeting());
+                    fm.greeting();
                     break;
                 case "exit":
-                    fm.print(fm.goodbye());
+                    fm.goodbye();
                     martIsOpen = false;
                     break;
                 default:
---
+++ D3/cart/src/main/java/swf/d3/ShoppingCart.java
@@ -0,0 +1,69 @@
+package swf.d3;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+
+    private String userName;
+    private ArrayList<String> cart;
+
+    public ShoppingCart() {
+        cart = new ArrayList<String>();
+        populateSampleData();
+    }
+
+    public boolean addToCart(String item) {
+
+        boolean addedToCart = true;
+
+        for (int i = 0; i < size(); i++) {
+            if (cart.get(i).equals(item)) {
+                addedToCart = false;
+                break;
+            }
+        }
+        if (addedToCart) {
+            cart.add(item);
+        }
+
+        return addedToCart;
+    }
+
+    public int size() {
+        return cart.size();
+    }
+
+    public String get(int index) {
+        return cart.get(index);
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+
+    private void populateSampleData() {
+        addToCart("apple");
+        addToCart("orange");
+        addToCart("banana");
+        addToCart("pear");
+        addToCart("kiwi");
+    }
+
+    public String removeFromCart(int index) {
+        String removed = null;
+        if (index <= size() && index > 0) {
+            index--;
+            removed = cart.get(index);
+            cart.remove(index);
+        }
+        return removed;
+    }
+
+    public void setUsername(String name){
+        this.userName = name;
+    }
+
+    public static void main(String[] args) {
+
+    }    
+}
