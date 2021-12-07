858422ceac8faa55de9b4867940b84b9b821744b
Gerald Yeo <ywz.gerald@gmail.com>
2021-12-07 16:17:31 +0800

    Initial implementation of JSON conversion.

--- D3/cart/src/main/java/swf/d3/ShoppingCartDB.java
+++ D3/cart/src/main/java/swf/d3/ShoppingCartDB.java
@@ -1,6 +1,8 @@
 package swf.d3;
 
 import java.util.ArrayList;
+//import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class ShoppingCartDB {
 
@@ -23,13 +25,27 @@ public class ShoppingCartDB {
         this.filePath = filePath;
     }
 
+    private JSONObject toJSON(ShoppingCart sc){
+        JSONObject obj = new JSONObject();
+        obj.put("User", sc.getUsername());
+        obj.put();
+        obj.put();
+        obj.put();
+
+    }
+
     public String getFilePath(){
         return filePath;
     }   
+
+    public boolean doesUserExist(){
+        boolean userExists = true;
+        // To Do
+        return userExists;
+    }
 
     public ArrayList<String> getUsers(){
         // To Do
         return new ArrayList<String>();
     }
-
 }
