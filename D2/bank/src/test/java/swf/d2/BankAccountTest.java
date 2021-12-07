af8b4cf178ee5fa5e1103d0039d8cb9f8dba00b3
Gerald Yeo <ywz.gerald@gmail.com>
2021-12-07 11:22:28 +0800

    Initial implementation of unit tests.

--- D2/bank/src/test/java/swf/d2/BankAccountTest.java
+++ D2/bank/src/test/java/swf/d2/BankAccountTest.java
@@ -14,16 +14,16 @@ public class BankAccountTest
      */
     @Test
     public void testGetBalance(){
-        BankAccount acct = new BankAccount("Elon Musk", 23000000000f);
-        assertTrue(acct.getBalance() == 23000000000f);
+        BankAccount acct = new BankAccount("Elon Musk", 230f);
+        assertTrue(acct.getBalance() == 230f);
         System.out.println("GetBalance succeeded.");
     }
 
     @Test
     public void testDeposit(){
-        BankAccount acct = new BankAccount("Elon Musk", 23000000000f);
+        BankAccount acct = new BankAccount("Elon Musk", 230f);
         acct.deposit(15f);
-        assertTrue(acct.getBalance() == 230000000015f);
+        assertTrue(acct.getBalance() == 245f);
         System.out.println("GetBalance succeeded.");
     }
 }
