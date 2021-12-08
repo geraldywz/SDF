6a22be3c35e63142f1082129ec3d7028ad812e66
Gerald Yeo <ywz.gerald@gmail.com>
2021-12-08 16:26:09 +0800

    Initial implementation of Cooke.java.

--- D4/cookie/src/main/java/swf/d4/Cookie.java
+++ D4/cookie/src/main/java/swf/d4/Cookie.java
@@ -1,5 +1,18 @@
 package swf.d4;
 
 public class Cookie{
+    private string fortune;
+
+    public Cookie(String fortune){
+        this.fortune = fortune;
+    }
+
+    public void getFortune(){
+        return fortune;
+    }
+
+    public void setFortune(String fortune){
+        this.fortune = fortune;
+    }
 
 }
