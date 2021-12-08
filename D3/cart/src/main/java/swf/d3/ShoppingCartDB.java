a1196740f6012cc78de72030b1d9cede4eea86eb
Gerald Yeo <ywz.gerald@gmail.com>
2021-12-08 11:23:15 +0800

    Initial implementation of saveCart().

--- D3/cart/src/main/java/swf/d3/ShoppingCartDB.java
+++ D3/cart/src/main/java/swf/d3/ShoppingCartDB.java
@@ -14,10 +14,10 @@ public class ShoppingCartDB {
         carts = new ArrayList<ShoppingCart>();
     }
 
-    public ShoppingCart loadCart(String userName){
+    public ShoppingCart loadCart(String userName) {
         ShoppingCart sc = null;
-        for(int i=0;i<carts.size();i++){
-            if(carts.get(i).getUsername().equals(userName)){
+        for (int i = 0; i < carts.size(); i++) {
+            if (carts.get(i).getUsername().equals(userName)) {
                 sc = carts.get(i);
             }
         }
@@ -25,51 +25,59 @@ public class ShoppingCartDB {
     }
 
     public void saveCart(ShoppingCart sc){
-        // To Do
+        for (int i = 0; i < carts.size(); i++) {
+            if (carts.get(i).getUsername().equals(sc.getUsername())) {
+                carts.set(i, sc);
+                break;
+            }else{
+                carts.add(sc);
+            }
+        }
     }
 
-    public void setFilePath(String filePath){
+    public void setFilePath(String filePath) {
         this.filePath = filePath;
     }
 
-    private JSONObject cartToJSON(ShoppingCart sc){
+    private JSONObject cartToJSON(ShoppingCart sc) {
         JSONObject obj = new JSONObject();
         obj.put("User", sc.getUsername());
 
         JSONArray items = new JSONArray();
-        for(int i=0;i<sc.size();i++){
+        for (int i = 0; i < sc.size(); i++) {
             items.put(sc.get(i));
         }
-        obj.put("Items",items);
+        obj.put("Items", items);
 
         return obj;
     }
 
-    private ShoppingCart jsonToCart(JSONObject obj){
-        ShoppingCart sc = new ShoppingCart((String)obj.get("User"));
-        
+    private ShoppingCart jsonToCart(JSONObject obj) {
+        ShoppingCart sc = new ShoppingCart((String) obj.get("User"));
+
         JSONArray items = (JSONArray) obj.get("Items");
-        for(int i=0;i<items.length();i++){
-            sc.addToCart((String)items.get(i));
+        for (int i = 0; i < items.length(); i++) {
+            sc.addToCart((String) items.get(i));
         }
         return sc;
     }
 
-    public String getFilePath(){
+    public String getFilePath() {
         return filePath;
-    }   
+    }
 
-    public boolean doesUserExist(){
+    public boolean doesUserExist() {
         boolean userExists = true;
         // To Do
         return userExists;
     }
 
-    public ArrayList<String> getUsers(){
+    public ArrayList<String> getUsers() {
         // To Do
         return new ArrayList<String>();
     }
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         ShoppingCart sc = new ShoppingCart("John");
         sc.populateSampleData();
         ShoppingCartDB scdb = new ShoppingCartDB("Test");
@@ -77,7 +85,7 @@ public class ShoppingCartDB {
         System.out.println(obj);
         sc = scdb.jsonToCart(obj);
         System.out.println(sc.getUsername());
-        for(int i=0;i<sc.size();i++){
+        for (int i = 0; i < sc.size(); i++) {
             System.out.println(sc.get(i));
         }
     }
