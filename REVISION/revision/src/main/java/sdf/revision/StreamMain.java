83f4e4b65bbc10b43fa6579a04ff520a234d4a36
Gerald Yeo <ywz.gerald@gmail.com>
2022-01-04 17:12:28 +0800

    Tidying up code from Revision.

--- REVISION/revision/src/main/java/sdf/revision/StreamMain.java
+++ REVISION/revision/src/main/java/sdf/revision/StreamMain.java
@@ -11,50 +11,64 @@ public class StreamMain {
      */
 
     public static void main(String[] args) {
-        List<String> words = Arrays.asList("big", "bog", "balloon", "boot", "bone", "bane", "bolt");
+        List<String> words = Arrays.asList("big", "bog", "balloon", "ballast", "boner", "banes", "bolter");
         List<String> uppercase = new LinkedList<>();
 
         uppercase = filterCollection(words); // Using Collections.
         // uppercase = filterStream(words); // Using Collections.
 
-        for (String s : words) {
+        for (String s : words) { // Raw data set
             System.out.println(s);
         }
-        System.out.println();
-        for (String s : uppercase) {
+
+        System.out.println(); // Link break.
+
+        for (String s : uppercase) {                        // Filtered using traditional Java syntax
             System.out.println(s);
         }
+
+        System.out.println(); // Link break.
+
+        uppercase = filterStream(words);
+        for (String s : uppercase) {                        // Same function as above but with lambda expressions.
+            System.out.println(s);
+        }
+
+        System.out.println(); // Link break.
+
+        System.out.println(orderedFilterStream(words));     // With lambda expressions, sorted in reverse order of input.
+
     }
 
-    public static List<String> orderedFilterStream(List<String> words) {
-        // Declarative approach, just give me the next item.
-        List<String> output = words.stream()
+    public static String orderedFilterStream(List<String> words) {
+        String output = words.stream()
                 .filter(v -> v.length() > 3)        // Arbitrary filter(mask)
                 .map(String::toUpperCase)           // Transformation
-                .reduce("", (s0, s1));
+                .reduce("", (s0, s1) -> "%s %s".formatted(s1, s0));
         return output;
     }
 
     public static List<String> filterStream(List<String> words) {
+        // Declarative approach, just give me the next item.
+
         List<String> output = words.stream()
-                .filter(v -> v.length() > 3) // Arbitrary filter(mask)
-                .map(String::toUpperCase) // Transformation
+                .filter(v -> v.length() > 3)        // Arbitrary filter(mask)
+                .map(String::toUpperCase)           // Transformation
                 .toList();
         return output;
     }
 
     public static List<String> filterCollection(List<String> words) {
         // Imperative approach, all actions must be explciitly stated.
+
         List<String> output = new LinkedList<>();
 
         for (String s : words) {
 
-            // Arbitrary filter
-            if (s.length() < 4) {
+            if (s.length() < 4) {                   // Arbitrary filter
                 continue;
-            }
-            // Transformation
-            output.add(s.toUpperCase());
+            }            
+            output.add(s.toUpperCase());            // Transformation
         }
         return output;
     }
